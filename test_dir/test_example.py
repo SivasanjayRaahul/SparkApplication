c40fe848a47fa7274c1e7f8ee20faa7e860e6100
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2024-05-27 12:28:31 +0530

    Add spark configs

--- test_dir/test_example.py
+++ test_dir/test_example.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lib import utils
+from lib import utils, config_loader
 
 
 @pytest.fixture(scope="session")
@@ -8,5 +8,25 @@ def spark():
     return utils.get_spark_session()
 
 
+@pytest.fixture(scope="session")
+def env():
+    return "LOCAL"
+
+
 def test_blank_test(spark):
     assert spark.version == "3.0.2"
+
+
+def test_spark_local_config_loader(env):
+    configs = config_loader.get_spark_conf(env)
+    assert configs.get("spark.app.name") == "sbdl-local"
+
+
+def test_spark_prod_config_loader():
+    configs = config_loader.get_spark_conf("PROD")
+    assert configs.get("spark.app.name") == "sbdl"
+    assert configs.get("spark.executor.cores") == "4"
+    assert configs.get("spark.executor.memory") == "10GB"
+    assert configs.get("spark.executor.memoryoverhead") == "1GB"
+    assert configs.get("spark.executor.instances") == "2"
+    assert configs.get("spark.sql.shuffle.partitions") == "8"
