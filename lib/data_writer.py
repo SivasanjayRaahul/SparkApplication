3e98e3d9482c5f76409dbab946253d3ef1accb24
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2024-05-29 11:41:55 +0530

    Update. data writer as overwrite

--- lib/data_writer.py
+++ lib/data_writer.py
@@ -4,4 +4,4 @@ from pyspark.sql import DataFrame
 def write(env: str, df: DataFrame, date):
     if env == "PROD":
         prefix = f"s3://videodatacount/timestamp={date}/"
-        df.write.csv(prefix)
+        df.write.mode("overwrite").csv(prefix)
